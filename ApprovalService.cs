a69a7b911d3536a5a0e1d6f36c86f4b13c385b52
ST10407732 <131349864+ST10407732@users.noreply.github.com>
2024-09-30 20:36:04 +0200

    Add files via upload

---
+++ ApprovalService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2BIceTask2
+{
+    public class ApprovalService
+    {
+
+        public void ProcessPayment(Payment payment, object approver)
+        {
+            var approveMethod = approver.GetType().GetMethod("ApprovePayment");
+            var result = (bool)approveMethod.Invoke(approver, new object[] { payment });
+
+            payment.Status = result ? Status.Approved : Status.Rejected;
+        }
+    }
+}
